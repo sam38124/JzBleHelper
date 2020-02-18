0977aad39d3f73df8947f88c0e84f0cf64f8351d
sam38124 <sam38124@gmail.com>
2020-02-17 23:18:38 -0800

    4.3

--- jzblehelper/src/main/java/com/jianzhi/jzblehelper/BleHelper.kt
+++ jzblehelper/src/main/java/com/jianzhi/jzblehelper/BleHelper.kt
@@ -44,7 +44,6 @@ class BleHelper(val context: Context, val callback: BleCallBack) {
             handler.post {
                 if (!bleServiceControl.isconnect) {
                     callback.onConnectFalse()
-                    closeBle()
                 }
             }
             stopScan()
